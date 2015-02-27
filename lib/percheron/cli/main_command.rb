a37fe92df48e0e622f2b8eead01aa0c66ea5ee81
Ash McKenzie <ash@the-rebellion.net>
2015-02-27 17:18:53 +1100

    MainCommand needs to < AbstractCommand so -c is available to all subcommands

--- lib/percheron/cli/main_command.rb
+++ lib/percheron/cli/main_command.rb
@@ -1,6 +1,6 @@
 module Percheron
   module CLI
-    class MainCommand < Clamp::Command
+    class MainCommand < AbstractCommand
       subcommand 'list', "List stacks and it's containers", ListCommand
       subcommand 'start', 'Start a stack', StartCommand
     end
