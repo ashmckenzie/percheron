de92b17e9f0738d552fc5f15f1aaed197981d1b8
Ash McKenzie <ash@the-rebellion.net>
2015-03-02 14:48:38 +1100

    Allow config file to be specified in PERCHERON_CONFIG env var

--- lib/percheron/cli/abstract_command.rb
+++ lib/percheron/cli/abstract_command.rb
@@ -2,11 +2,17 @@ module Percheron
   module CLI
     class AbstractCommand < Clamp::Command
 
-      option [ '-c', '--config_file' ], 'CONFIG', 'Configuration file', default: '.percheron.yml'
+      DEFAULT_CONFIG_FILE = '.percheron.yml'
+
+      option [ '-c', '--config_file' ], 'CONFIG', 'Configuration file', default: DEFAULT_CONFIG_FILE
 
       option '--version', :flag, 'show version' do
         puts Percheron::VERSION
         exit(0)
+      end
+
+      def default_config_file
+        ENV.fetch('PERCHERON_CONFIG', DEFAULT_CONFIG_FILE)
       end
 
       def config
