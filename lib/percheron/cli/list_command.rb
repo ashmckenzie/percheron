f48ee1c0c78c1c16adc04e056ca8771a5791a8d5
Ash McKenzie <ash@the-rebellion.net>
2015-03-04 00:33:53 +1100

    list subcommand now accepts an optional stack_name

--- lib/percheron/cli/list_command.rb
+++ lib/percheron/cli/list_command.rb
@@ -2,8 +2,17 @@ module Percheron
   module CLI
     class ListCommand < AbstractCommand
 
+      parameter 'STACK_NAME', 'stack name', required: false
+
       def execute
-        Stack.all(config).each do |stack_name, stack|
+        stacks = if stack_name
+          Stack.get(config, stack_name)
+        else
+          Stack.all(config)
+        end
+
+        stacks.each do |stack_name, stack|
+          puts
           puts Percheron::Formatters::Stack::Table.new(stack).generate
         end
       end
