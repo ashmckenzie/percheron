93e57827b1e58034bde7fac8099efefc8a8f6055
Ash McKenzie <ash@the-rebellion.net>
2015-04-23 08:08:29 +1000

    Ensure stack is valid before displaying table

--- lib/percheron/commands/list.rb
+++ lib/percheron/commands/list.rb
@@ -5,10 +5,13 @@ module Percheron
       parameter('STACK_NAME', 'stack name', required: false)
 
       def execute
-        super
         Stack.get(config, stack_name).each do |_, stack|
-          puts
-          puts Percheron::Formatters::Stack::Table.new(stack).generate
+          begin
+            stack.valid?
+            puts("\n", Percheron::Formatters::Stack::Table.new(stack).generate)
+          rescue Percheron::Errors::StackInvalid => e
+            signal_usage_error(e.message)
+          end
         end
       end
     end
