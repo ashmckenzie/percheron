c331cd0241b5fde2607eb25fd863fdeeccfc3c33
Ash McKenzie <ash@the-rebellion.net>
2015-08-18 17:24:02 +1000

    Nicer logging when using Build action

--- lib/percheron/actions/build.rb
+++ lib/percheron/actions/build.rb
@@ -68,7 +68,7 @@ module Percheron
             execute_pre_build_scripts!
             $logger.info "Building '#{unit.image_name}' image"
             Connection.perform(Docker::Image, :build_from_dir, base_dir, options) do |out|
-              $logger.info '%s' % [ out.strip ]
+              $logger.info '%s' % [ JSON.parse(out)['stream'].strip ]
             end
           end
         ensure
